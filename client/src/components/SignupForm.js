31710801502dad6a0b7cbc847457e542c496ef9a
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-12-08 19:35:26 -0800

    refactored signup form with createUser mutation

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+
+import { CREATE_USER } from '../utils/mutation';
 
 const SignupForm = () => {
   // set initial form state
@@ -11,6 +13,9 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  //use Mutation instead of API routes
+  const [createUser, { error, data }] = useMutation(CREATE_USER)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,7 +33,13 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await createUser({
+        variables: {
+          username: userFormData.username,
+          email: userFormData.email,
+          password: userFormData.password,
+        }
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
