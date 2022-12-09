c32271f7a170b9672a510bdc1f7598ae4327ef59
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-12-08 17:42:34 -0800

    finished factoring server.js

--- server/server.js
+++ server/server.js
@@ -9,10 +9,18 @@ const { ApolloServer } = require('apollo-server-express');
 //importing schemas from graphQL
 const { typeDefs, resolvers } = require('./schemas');
 
-//
+//auth middleware for JWT
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//setting up Apollo Server + auth
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,9 +29,24 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+//commenting out because we are now using Apollo Server instead of our regular express
+// app.use(routes);
 
-app.use(routes);
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+//server open
+const startApolloServer = async (typeDefs, resovlers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API Server on PORT ${PORT}`);
+      coneole.log(`GraphQL @ http://localhost:${PORT}`)
+    });
+  });
+};
+
+startApolloServer(typeDefs, resolvers);
